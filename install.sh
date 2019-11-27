c30dc1bd61c2d26190263463afd7d4b9115c1c5b
kittichai14341 <kittichai.p@facgure.com>
2019-11-27 19:07:26 +0700

    kittichai

--- install.sh
+++ install.sh
@@ -24,9 +24,9 @@ echo "Ipdocker = ${Ipdocker}"
 
 if [ `id -u` -ne 0 ] ; then echo "Please run as root" ; exit 1 ; fi
 
-if [ -x "$(command -v apt-get)" ]; then apt-get install unzip jq ; fi
+if [ -x "$(command -v apt-get)" ]; then apt-get -y install unzip jq ; fi
 
-if [ -x "$(command -v yum)" ]; then sudo yum install unzip xinetd redhat-lsb-core nmap jq ; fi
+if [ -x "$(command -v yum)" ]; then sudo yum -y install unzip xinetd redhat-lsb-core nmap jq ; fi
 
 wget https://github.com/kittichai14341/Prometheus-exporter/raw/master/prometheus-exporter.zip -O /tmp/prometheus-exporter.zip
 
